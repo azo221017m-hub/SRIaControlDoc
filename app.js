7f8d123548c683581ac357d1e1cf9620999c4906
azo221017m-hub <azo221017m@gmail.com>
2025-08-08 02:41:46 -0600

    Subida forzada desde local

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta raíz para devolver index.html explícitamente
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
